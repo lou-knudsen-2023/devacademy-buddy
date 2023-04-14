c4a0b4d3d04b91eaccbd471fb8a3a414c541a34f
Emily <student@pop-os.localdomain>
2023-04-14 15:33:38 +1200

    apiClient finished

--- client/apis/apiClient.ts
+++ client/apis/apiClient.ts
@@ -1,26 +1,28 @@
 import request from 'superagent'
-import { UserData } from '../../models/Users'
+import { User } from '../../models/Users'
 
-export function getLocals(): Promise<UserData> {
-  return request.get(locals).then((res) => res.body)
+export function getAllUsers(): Promise<User[]> {
+  return request.get('/api/v1/buddy').then((res) => res.body)
 }
 
-export function delLocal(id) {
-  return request.delete(`${locals}${local.id}`).then((res) => {
-    return res.body
-  })
+export function deleteUser(id: number): Promise<User> {
+  return request.delete(`/api/v1/buddy${id}`).then((res) => res.body)
 }
 
-export function addLocal(newLocal: UserData) {
+export function addNewUser(newUser: User): Promise<User> {
   return request
-    .post()
-    .send(newLocal)
-    .then((res) => res.body)
+    .post('/api/v1/buddy')
+    .send(newUser)
+    .then((res) => {
+      return res.body
+    })
 }
 
-export function updateLocal(newLocal: UserData) {
+export function updateUser(newUser: User, id: number): Promise<User> {
   return request
-    .patch(`${locals}${newLocal.id}`)
-    .send(newLocal)
-    .then((res) => res.body[0])
+    .patch(`/api/v1/buddy${id}`)
+    .send(newUser)
+    .then((res) => {
+      return res.body
+    })
 }
